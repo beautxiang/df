5640bd688df0f1e24280379cfc4846863da93a52
章翔 <81078596+beautxiang@users.noreply.github.com>
2021-06-15 16:08:02 +0800

    Update settings.py

--- df/settings.py
+++ df/settings.py
@@ -38,6 +38,8 @@ REST_FRAMEWORK = {
     # 添加分页器配置项
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.AutoSchema',
 
 }
 
